a37d65a0a4bc7ba5b8a28c9ba48ef32f806ad36b
Mohamed Hamdi <haamdi@outlook.com>
2022-12-01 13:48:44 +0100

    Day01
    Signed-off-by: Mohamed Hamdi <haamdi@outlook.com>

--- src/test/java/org/aoc/Day01.java
+++ src/test/java/org/aoc/Day01.java
@@ -1,14 +1,29 @@
 package org.aoc;
 
+import one.util.streamex.StreamEx;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 class Day01 {
 
   @Test void firstPuzzle() throws IOException {
-    final List<String> input = Utils.readInput("Day01");
+    final var input = Utils.readInput("Day01");
+    System.out.println(getElves(input).max(Comparator.naturalOrder()).orElseThrow());
+  }
+  @Test void secondPuzzle() throws IOException {
+    final var input = Utils.readInput("Day01");
+    final Integer result =
+      getElves(input).reverseSorted().limit(3).reduce(Integer::sum).orElseThrow();
+    System.out.println(result);
+  }
 
+  private static StreamEx<Integer> getElves(List<String> input) {
+    final StreamEx<List<String>> lists =
+      StreamEx.of(input).groupRuns((s, s2) -> !s.isEmpty());
+    return lists.map(strings -> strings.stream().filter(s -> !s.isEmpty()))
+      .map(it -> it.map(Integer::parseInt).reduce(Integer::sum).orElseThrow());
   }
 }
